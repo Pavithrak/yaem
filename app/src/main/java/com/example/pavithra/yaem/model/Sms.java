4277f3365776b5eac68f968941bfb6927cd6270c
Pavithra <pavithra@thoughtworks.com>
2018-01-14 15:26:02 +0530

    Implementation to get withdrawal and credited amount from sms body

--- app/src/main/java/com/example/pavithra/yaem/model/Sms.java
+++ app/src/main/java/com/example/pavithra/yaem/model/Sms.java
@@ -1,14 +1,24 @@
 package com.example.pavithra.yaem.model;
 
+import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Sms {
     private String address;
     private String body;
     private Date date;
-    private final List<String> withdrawalRegex = new ArrayList<>();
+    private final List<Pattern> withdrawalRegex = new ArrayList<Pattern>() {{
+        add(Pattern.compile("debited .* by (INR\\s|Rs.)(\\d[^\\s]*)"));
+        add(Pattern.compile("of (INR\\s|Rs.)(\\d[^\\s]*).*Credit Card"));
+    }};
+    private final List<Pattern> creditRegex = new ArrayList<Pattern>() {{
+        add(Pattern.compile("credited by (INR\\s|Rs.)(\\d[^\\s]*)"));
+    }};
 
     public Sms(String address, String body, Date date) {
         this.address = address;
@@ -30,6 +40,33 @@ public class Sms {
     }
 
     public Double getWithdrawlAmount() {
-        return null;
+        return this.getAmount(this.withdrawalRegex);
+    }
+
+    public Double getCreditedAmount() {
+        return this.getAmount(this.creditRegex);
+    }
+
+    private Double getAmount(List<Pattern> patterns) {
+        Double value = null;
+        for(Pattern pattern : patterns) {
+            Matcher matcher = pattern.matcher(this.body);
+            if(matcher.find()) {
+                String numberString = matcher.group(2);
+                value = convertStringToDouble(numberString);
+                break;
+            }
+        }
+        return value;
+    }
+
+    private Double convertStringToDouble(String number) {
+        Double value;
+        try {
+            value = NumberFormat.getInstance().parse(number).doubleValue();
+        } catch (ParseException e) {
+            value = null;
+        }
+        return value;
     }
 }
--- app/src/test/java/com/example/pavithra/yaem/model/SmsTest.java
+++ app/src/test/java/com/example/pavithra/yaem/model/SmsTest.java
@@ -11,7 +11,7 @@ public class SmsTest {
     @Test
     public void shouldGetWithdrawnAmount() throws Exception {
         Sms sms = new Sms("AD-BANK",
-                "Your a/c XX1234 is debited on 09/01/2018 by INR 3,144.95 towards Purchase. Avl Bal : INR 123456. For more details login to m.sc.com/in - StanChart",
+                "Your a/c XX1234 is debited on 09/01/2018 by INR 3,144.95 towards Purchase. Avl Bal : INR 123. For more details login to m.sc.com/in - StanChart",
                 new Date());
         Double withdrawalAmount = sms.getWithdrawlAmount();
         assertEquals(new Double(3144.95), withdrawalAmount);
@@ -20,19 +20,31 @@ public class SmsTest {
                 "Tranx of INR 759.50 using Credit Card xxx1234 is made at VODAFONE-BILLDE on 10-JAN-18. Avbl Cr lmt:INR 63,488.50, Total Cr lmt: INR 70,000.00",
                 new Date());
         withdrawalAmount = sms.getWithdrawlAmount();
-        assertEquals(new BigDecimal(759.50),withdrawalAmount);
+        assertEquals(new Double(759.50),withdrawalAmount);
 
         sms = new Sms("AD-BANK",
                 "Dear Customer, Your a/c no. XXXXXXXX1234 is credited by Rs.20,000.00 on 11-Jan-2018 12:41:56 by a/c linked to mobile XXXXX61234. (IMPS Ref no 12345).",
                 new Date());
 
         withdrawalAmount = sms.getWithdrawlAmount();
-        assertEquals(new BigDecimal(0),withdrawalAmount);
+        assertNull(withdrawalAmount);
     }
 
     @Test
     public void shouldGetCreditedAmount() throws Exception {
+        Sms sms = new Sms("AD-BANK",
+                "Dear Customer, Your a/c no. XXXXXXXX1234 is credited by Rs.20,000.00 on 11-Jan-2018 12:41:56 by a/c linked to mobile XXXXX61234. (IMPS Ref no 12345).",
+                new Date());
 
+        Double creditedAmount = sms.getCreditedAmount();
+        assertEquals(new Double(20000), creditedAmount);
+
+        sms = new Sms("AD-BANK",
+                "Tranx of INR 759.50 using Credit Card xxx1234 is made at VODAFONE-BILLDE on 10-JAN-18. Avbl Cr lmt:INR 63,488.50, Total Cr lmt: INR 70,000.00",
+                new Date());
+
+        creditedAmount = sms.getCreditedAmount();
+        assertNull(creditedAmount);
     }
 
     @Test
