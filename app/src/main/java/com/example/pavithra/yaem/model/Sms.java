4d00f9ce81f45350ee1cae7f04fca26736158637
Pavithra <pavithra@thoughtworks.com>
2018-01-15 12:41:49 +0530

    Get trasaction date from sms body

--- app/src/main/java/com/example/pavithra/yaem/model/Sms.java
+++ app/src/main/java/com/example/pavithra/yaem/model/Sms.java
@@ -2,7 +2,9 @@ package com.example.pavithra.yaem.model;
 
 import java.text.NumberFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -19,6 +21,17 @@ public class Sms {
     private final List<Pattern> creditRegex = new ArrayList<Pattern>() {{
         add(Pattern.compile("credited by (INR\\s|Rs.)(\\d[^\\s]*)"));
     }};
+    private final List<Pattern> dateRegex = new ArrayList<Pattern>() {{
+        add(Pattern.compile("on.([^\\s|.]*)"));
+    }};
+    private final List<String> dateFormat = new ArrayList<String>() {{
+        add("dd-MM-yyyy");
+        add("dd-MMM-yyyy");
+        add("dd-MMM-yy");
+        add("dd/MM/yyyy");
+        add("dd/MMM/yyyy");
+        add("dd/MMM/yy");
+    }};
 
     public Sms(String address, String body, Date date) {
         this.address = address;
@@ -30,13 +43,8 @@ public class Sms {
         return address;
     }
 
-    @Override
-    public String toString() {
-        return "Sms{" +
-                "address='" + address + '\'' +
-                ", body='" + body + '\'' +
-                ", date=" + date +
-                '}';
+    public String getBody() {
+        return body;
     }
 
     public Double getWithdrawlAmount() {
@@ -47,17 +55,71 @@ public class Sms {
         return this.getAmount(this.creditRegex);
     }
 
+    public Integer getTransactionMonth() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(this.getTransactionDate());
+        return calendar.get(Calendar.MONTH) + 1;
+    }
+
+    public Integer getTransactionYear() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(this.getTransactionDate());
+        int year = calendar.get(Calendar.YEAR);
+        return year < 100 ? year + 2000 : year;
+    }
+
+    public Integer getTransactionDay() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(this.getTransactionDate());
+        return calendar.get(Calendar.DATE);
+    }
+
+    @Override
+    public String toString() {
+        return "Sms{" +
+                "address='" + address + '\'' +
+                ", body='" + body + '\'' +
+                ", date=" + date +
+                '}';
+    }
+
+    private Date getTransactionDate() {
+        Date transDate = null;
+        for (Pattern pattern : this.dateRegex) {
+            Matcher matcher = pattern.matcher(this.body);
+            if (matcher.find()) {
+                String date = matcher.group(1);
+                transDate = convertStringToDate(date);
+                break;
+            }
+        }
+        return transDate;
+    }
+
     private Double getAmount(List<Pattern> patterns) {
         Double value = null;
-        for(Pattern pattern : patterns) {
+        for (Pattern pattern : patterns) {
             Matcher matcher = pattern.matcher(this.body);
-            if(matcher.find()) {
+            if (matcher.find()) {
                 String numberString = matcher.group(2);
                 value = convertStringToDouble(numberString);
                 break;
             }
         }
         return value;
+    }
+
+    private Date convertStringToDate(String date) {
+        Date parsedDate = null;
+        for (String dateFormat : this.dateFormat) {
+            SimpleDateFormat format = new SimpleDateFormat(dateFormat);
+            try {
+                parsedDate = format.parse(date);
+                break;
+            } catch (ParseException e) {
+            }
+        }
+        return parsedDate;
     }
 
     private Double convertStringToDouble(String number) {
--- app/src/test/java/com/example/pavithra/yaem/model/SmsTest.java
+++ app/src/test/java/com/example/pavithra/yaem/model/SmsTest.java
@@ -3,6 +3,7 @@ package com.example.pavithra.yaem.model;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import static org.junit.Assert.*;
@@ -49,11 +50,26 @@ public class SmsTest {
 
     @Test
     public void shouldGetWithdrawnDate() throws Exception {
+        Sms sms = new Sms("AD-BANK",
+                "Your a/c XX1234 is debited on 09/01/2018 by INR 3,144.95 towards Purchase. Avl Bal : INR 123. For more details login to m.sc.com/in - StanChart",
+                new Date());
+        assertEquals(2018, sms.getTransactionYear().intValue());
+        assertEquals(1, sms.getTransactionMonth().intValue());
+        assertEquals(9, sms.getTransactionDay().intValue());
 
-    }
+        sms = new Sms("AD-BANK",
+                "Tranx of INR 759.50 using Credit Card xxx1234 is made at VODAFONE-BILLDE on 10-JAN-18. Avbl Cr lmt:INR 63,488.50, Total Cr lmt: INR 70,000.00",
+                new Date());
+        assertEquals(2018, sms.getTransactionYear().intValue());
+        assertEquals(1, sms.getTransactionMonth().intValue());
+        assertEquals(10, sms.getTransactionDay().intValue());
 
-    @Test
-    public void shouldGetCategory() throws Exception {
+        sms = new Sms("AD-BANK",
+                "Dear Customer, Your a/c no. XXXXXXXX1234 is credited by Rs.20,000.00 on 11-Dec-2017 12:41:56 by a/c linked to mobile XXXXX61234. (IMPS Ref no 12345).",
+                new Date());
 
+        assertEquals(2017, sms.getTransactionYear().intValue());
+        assertEquals(12, sms.getTransactionMonth().intValue());
+        assertEquals(11, sms.getTransactionDay().intValue());
     }
 }
